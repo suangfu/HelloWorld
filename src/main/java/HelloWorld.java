021c31155156e6fb4cdc68d231defa9b39c0e084
SuangFu <suangfu@gmail.com>
2023-11-27 16:33:19 +0800

    Initialize Project

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Don't play play");
+        System.out.println("Java Gradle Working...");
+    }
+}
